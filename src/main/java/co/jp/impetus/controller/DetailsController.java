479b5130545c25a3f1982c24728a11bbe7b49a7d
Ken Tomonaga <tom@kenp.com>
2018-03-09 00:31:02 +0900

    サーバ→クライアント

--- src/main/java/co/jp/impetus/controller/DetailsController.java
+++ src/main/java/co/jp/impetus/controller/DetailsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import co.jp.impetus.dto.DetailsDto;
 import co.jp.impetus.dto.LoginDto;
 import co.jp.impetus.mapper.LoginMapper;
 
@@ -17,17 +18,38 @@ public class DetailsController {
     @Autowired
     LoginMapper mapper;
 
-    public static final String INPUT_COMMAND_NAME = "loginDto";
+    public static final String INPUT_COMMAND_NAME = "detailsDto";
 
     @ModelAttribute(INPUT_COMMAND_NAME)
     public LoginDto createLoginDto() {
         LoginDto dto = new LoginDto();
+
+
+
         return dto;
+    }
+
+    @ModelAttribute
+    private DetailsDto createDetailsDto() {
+
+    	//以下テスト用処理
+        DetailsDto detailsDto = new DetailsDto();
+
+        //とりあえず画面に出力する値たち
+        detailsDto.setProuductionName("リスアニ! Vol.32.1「アイドルマスター」音楽大全 永久保存版V");
+        detailsDto.setAuthorName("山崎はるか(春日未来役)/郁原ゆう(エミリー スチュアート役)/上田麗奈(高坂海美役)/駒形友梨(高山紗代子役)/伊藤美来(七尾百合子役)/村川梨衣(松田亜利沙役)/木戸衣吹(矢吹可奈役)/愛美(ジュリア役)/小岩井ことり(天空橋朋花役)/斉藤佑圭(永吉 昴役)/戸田めぐみ(舞浜 歩役)/阿部里果(真壁瑞希役)/山口立花子(百瀬莉緒役)/Machico(伊吹 翼役)/稲川英里(大神 環役)/田村奈央(木下ひなた役)/香里有佐(桜守歌織役)/小笠原早紀(野々原 茜役)/麻倉もも(箱崎星梨花役)");
+        detailsDto.setPublisherName("エムオン・エンタテインメント");
+        detailsDto.setIsbnCode("978-4789772778");
+
+        return detailsDto;
     }
 
     @RequestMapping(value = "/init", method = RequestMethod.GET)
     public String init(Model model) {
-        LoginDto dto = createLoginDto();
+
+        //LoginDto dto = createLoginDto();
+    	DetailsDto dto = createDetailsDto();
+
         model.addAttribute(INPUT_COMMAND_NAME, dto);
 
         return "details";
---
+++ src/main/java/co/jp/impetus/dto/DetailsDto.java
@@ -0,0 +1,106 @@
+package co.jp.impetus.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class DetailsDto implements Serializable {
+
+	/*ISBNコード*/
+	private String isbnCode ;
+
+	/*作品名*/
+	private String prouductionName ;
+
+	/*作者名*/
+	private String authorName ;
+
+	/*発行元*/
+	private String publisherName ;
+
+	/*発効年月日*/
+	private Date issuedDate ;
+
+	/*DBに項目はあるがモックに項目がないため、他の項目については割愛する*/
+
+
+	/**
+	 * ISBNコードを取得する
+	 * @return String isbnCode
+	 */
+	public String getIsbnCode() {
+		return isbnCode;
+	}
+
+	/**
+	 * ISBNコードを設定する
+	 * @param String isbnCode
+	 */
+	public void setIsbnCode(String isbnCode) {
+		this.isbnCode = isbnCode;
+	}
+
+	/**
+	 * 作品名を取得する
+	 * @return String prouductionName
+	 */
+	public String getProuductionName() {
+		return prouductionName;
+	}
+
+	/**
+	 * 作品名を設定する
+	 * @param String prouductionName
+	 */
+	public void setProuductionName(String prouductionName) {
+		this.prouductionName = prouductionName;
+	}
+
+	/**
+	 * 作者名を取得する
+	 * @return author_name
+	 */
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	/**
+	 * 作者名を設定する
+	 * @param author_name セットする author_name
+	 */
+	public void setAuthorName(String author_name) {
+		this.authorName = author_name;
+	}
+
+	/**
+	 * 発行元を取得する
+	 * @return publisherName
+	 */
+	public String getPublisherName() {
+		return publisherName;
+	}
+
+	/**
+	 * 発行元を設定する
+	 * @param publisherName セットする publisherName
+	 */
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+
+	/**
+	 * 発効年月日を取得する
+	 * @return issuedDate
+	 */
+	public Date getIssuedDate() {
+		return issuedDate;
+	}
+
+	/**
+	 * 発効年月日を設定する
+	 * @param issuedDate セットする issuedDate
+	 */
+	public void setIssuedDate(Date issuedDate) {
+		this.issuedDate = issuedDate;
+	}
+
+}
